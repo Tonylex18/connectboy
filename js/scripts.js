a05c4cf36137c908622c5e32845a486a91343e5a
Tonylex18 <anthonyagada2000@gmail.com>
2023-02-24 21:36:02 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     window.addEventListener("scroll", function () {
+//         if (window.scrollY > 50) {
+//             document.getElementById("navbar").classList.add("fixed-top");
+//             document.getElementById("navbar").style.display = "none";
+//             // add padding top to show content behind navbar
+//             navbar_height = document.querySelector(".navbar").offsetHeight;
+//             //document.body.style.paddingTop = navbar_height + "px";
+//         } else {
+//             document.getElementById("navbar").classList.remove("fixed-top");
+//             document.getElementById("navbar").style.display = "block";
+//             // remove padding top from body
+//             //document.body.style.paddingTop = "0";
+//         }
+//     });
+// });
+// // DOMContentLoaded  end
